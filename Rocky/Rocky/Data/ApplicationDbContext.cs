78ab0649b08e451ec0c1b9fd44b16b96a1dbfbea
Rezo Mishvelidze <rezo.mishvelidze@gmail.com>
2021-08-05 15:54:58 +0400

    Application DbContext

---
+++ Rocky/Rocky/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rocky.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
