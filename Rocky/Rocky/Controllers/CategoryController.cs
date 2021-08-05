d7eb538d707d6eb02c7d3b3b8d45301188485455
Rezo Mishvelidze <rezo.mishvelidze@gmail.com>
2021-08-05 17:04:41 +0400

    take some data from data base

--- Rocky/Rocky/Controllers/CategoryController.cs
+++ Rocky/Rocky/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
 
 namespace Rocky.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Category; 
+            return View(objList);
         }
     }
 }
