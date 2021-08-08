8bff1a5411fcb7b7ca43c562001f14fbeebda1bd
Rezo Mishvelidze <rezo.mishvelidze@gmail.com>
2021-08-08 12:33:10 +0400

    POST - EDIT and view of this

--- Rocky/Rocky/Controllers/CategoryController.cs
+++ Rocky/Rocky/Controllers/CategoryController.cs
@@ -39,5 +39,21 @@ namespace Rocky.Controllers
             }
             return View(obj);
         }
+
+        //POST - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
